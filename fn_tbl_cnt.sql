c2e37f938142cdcf3b3b572cf5b3cfe3536b1e2a
Sumin <126481278+xxumin@users.noreply.github.com>
2024-01-09 11:14:44 +0900

    Update fn_tbl_cnt.sql

--- fn_tbl_cnt.sql
+++ fn_tbl_cnt.sql
@@ -1,4 +1,4 @@
-create or replace function fn_tbl_cnt(tbl_nm varchar(50)) returns integer as $$
+create or replace function fn_tbl_cnt(tbl_nm varchar(50) default null) returns integer as $$
 declare
 cnt_sql text;
 rtn_cnt int := 0;
@@ -10,12 +10,20 @@ begin
 end;
 $$ language plpgsql;
 
-create or replace function sp_tbl_cnt(tbl_name varchar(50)) returns void as $$
+create or replace procedure sp_tbl_cnt(in tbl_name varchar(50) default null) as $$
 declare
 cnt_sql text;
 rtn_cnt int := 0;
 begin
+  if tbl_name is null then
+    return;
+  end if;
+
   cnt_sql := 'select count(1) from ods.' || tbl_name;
-  execute cnt_sql;
+  execute cnt_sql into rtn_cnt;
+
+  update mig_list
+  set cnt = rtn_cnt
+  where tbl_nm = tbl_name;
 end;
 $$ language plpgsql;
